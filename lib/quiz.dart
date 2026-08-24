3b61a7190b510ac437e275c80bed5d7f03468bea
gabrzb <gabrielzbz.gz@gmail.com>
2026-08-24 17:30:01 -0300

    fix: updated method to lift up states

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -20,6 +20,12 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'question-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+    
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -33,9 +39,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
