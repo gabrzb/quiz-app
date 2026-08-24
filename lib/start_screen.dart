83903a150ea5e3d0c10e53045e405841d9d4ae5e
gabrzb <gabrielzbz.gz@gmail.com>
2026-08-24 16:43:42 -0300

    feat: updated Start Screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,20 +9,25 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png', 
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255)
+          ),
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
             style: TextStyle(color: Colors.white, fontSize: 24),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {}, 
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
               textStyle: const TextStyle(fontSize: 20),
             ),
-            child: Text('Start Quiz' ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz' ),
           ),
         ],
       ),
